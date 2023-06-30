b8115c7b4fdab51fb1ba96e6f63fe7a716296bed
lighttigerxiv <lighttigerxiv@gmail.com>
2023-06-30 20:27:10 +0100

    made library

---
+++ src/actions/mod.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ResultAction {
+    OpenApp(OpenApp),
+    OpenInBrowser(OpenInBrowser),
+    CopyToClipboard(CopyToClipboard),
+    ExtensionAction(ExtensionAction),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct OpenApp {
+    pub desktop_path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct OpenInBrowser {
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CopyToClipboard {
+    pub text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ExtensionAction {
+    pub extension_name: String,
+    pub args: String,
+}
+
+impl OpenApp {
+    pub fn new(desktop_path: String) -> Self {
+        return OpenApp { desktop_path };
+    }
+}
---
+++ src/results/mod.rs
@@ -0,0 +1,64 @@
+use crate::actions::ResultAction;
+use serde::Serialize;
+use serde::Deserialize;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TextResult {
+    pub text: String,
+    pub action: ResultAction,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IconWithTextResult {
+    pub icon: String,
+    pub text: String,
+    pub action: ResultAction,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TitleAndDescriptionResult {
+    pub title: String,
+    pub description: String,
+    pub action: ResultAction,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IconWithTitleAndDescriptionResult {
+    pub icon: String,
+    pub title: String,
+    pub description: String,
+    pub action: ResultAction,
+}
+
+impl TextResult {
+    pub fn new(text: String, action: ResultAction) -> Self {
+        return TextResult { text, action };
+    }
+}
+
+impl IconWithTextResult {
+    pub fn new(icon: String, text: String, action: ResultAction) -> Self {
+        return IconWithTextResult { icon, text, action };
+    }
+}
+
+impl TitleAndDescriptionResult {
+    pub fn new(title: String, description: String, action: ResultAction) -> Self {
+        return TitleAndDescriptionResult {
+            title,
+            description,
+            action,
+        };
+    }
+}
+
+impl IconWithTitleAndDescriptionResult {
+    pub fn new(icon:String, title: String, description: String, action: ResultAction) -> Self {
+        return IconWithTitleAndDescriptionResult {
+            icon,
+            title,
+            description,
+            action,
+        };
+    }
+}
