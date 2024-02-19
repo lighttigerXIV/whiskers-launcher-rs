54b87591cb29666fb14cb08146ce5fd2da488a87
lighttigerxiv <lighttigerxiv@gmail.com>
2024-02-19 22:59:40 +0000

    remove returns

--- src/paths/mod.rs
+++ src/paths/mod.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 pub fn get_local_dir() -> Option<PathBuf> {
-    return match env::consts::OS {
+    match env::consts::OS {
         "windows" => {
             let mut path = Path::new(&env::var("APPDATA").unwrap()).to_owned();
             path.push("com-lighttigerxiv-whiskers-launcher");
@@ -17,11 +17,11 @@ pub fn get_local_dir() -> Option<PathBuf> {
 
             Some(path)
         }
-    };
+    }
 }
 
 pub fn get_temp_dir() -> Option<PathBuf> {
-    return match env::consts::OS {
+    match env::consts::OS {
         "windows" => {
             let mut tmp_path = Path::new(&env::var("TEMP").unwrap()).to_owned();
             tmp_path.push("whiskers-launcher-runtime");
@@ -30,11 +30,11 @@ pub fn get_temp_dir() -> Option<PathBuf> {
         }
         //Unix Systems
         _ => Some(Path::new("/tmp/whiskers-launcher-runtime").to_owned()),
-    };
+    }
 }
 
 pub fn get_home_dir() -> Option<PathBuf> {
-    return home_dir();
+    home_dir()
 }
 
 pub fn get_extension_results_path() -> Option<PathBuf> {
@@ -46,14 +46,14 @@ pub fn get_extension_results_path() -> Option<PathBuf> {
     };
 
     path.push("extension_results.json");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_extension_context_path() -> Option<PathBuf> {
     let mut path = get_temp_dir()?;
 
     path.push("extension_context.json");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_dialog_results_path() -> Option<PathBuf> {
@@ -65,59 +65,59 @@ pub fn get_dialog_results_path() -> Option<PathBuf> {
     };
 
     path.push("dialog_results.json");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_user_extensions_dir() -> Option<PathBuf> {
     let mut path = get_local_dir()?;
     path.push("UserExtensions");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_indexing_dir() -> Option<PathBuf> {
     let mut path = get_local_dir()?;
     path.push("Indexing");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_indexing_shortcuts_dir() -> Option<PathBuf> {
     let mut path = get_indexing_dir()?;
     path.push("Shortcuts");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_indexing_icons_dir() -> Option<PathBuf> {
     let mut path = get_indexing_dir()?;
     path.push("Icons");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_indexing_extensions_path() -> Option<PathBuf> {
     let mut path = get_indexing_dir()?;
     path.push("extensions.json");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_indexing_apps_path() -> Option<PathBuf> {
     let mut path = get_indexing_dir()?;
     path.push("apps.json");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_app_resources_dir() -> Option<PathBuf> {
     let mut path = get_local_dir()?;
     path.push("AppResources");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_app_resources_icons_dir() -> Option<PathBuf> {
     let mut path = get_app_resources_dir()?;
     path.push("Icons");
-    return Some(path);
+    Some(path)
 }
 
 pub fn get_settings_path() -> Option<PathBuf> {
-    return match env::consts::OS {
+    match env::consts::OS {
         "windows" => {
             let mut path = get_local_dir()?;
             path.push("settings.json");
@@ -131,11 +131,11 @@ pub fn get_settings_path() -> Option<PathBuf> {
 
             Some(path)
         }
-    };
+    }
 }
 
 pub fn get_autostart_path() -> Option<PathBuf> {
-    return match env::consts::OS {
+    match env::consts::OS {
         "linux" => {
             let mut path = get_home_dir()?;
             path.push(".config/autostart");
@@ -149,36 +149,36 @@ pub fn get_autostart_path() -> Option<PathBuf> {
             return Some(path);
         }
         _ => None,
-    };
+    }
 }
 
-pub fn get_extension_dialog_action_path()-> Option<PathBuf>{
+pub fn get_extension_dialog_action_path() -> Option<PathBuf> {
     let mut path = get_temp_dir()?;
     path.push("extension_dialog_action.json");
 
-    return Some(path); 
+    Some(path)
 }
 
-pub fn get_extension_dialog_response_path()-> Option<PathBuf>{
+pub fn get_extension_dialog_response_path() -> Option<PathBuf> {
     let mut path = get_temp_dir()?;
     path.push("extension_dialog_response.json");
 
-    return Some(path); 
+    Some(path)
 }
 
-pub fn get_store_cache_dir() -> Option<PathBuf>{
+pub fn get_store_cache_dir() -> Option<PathBuf> {
     let mut path = get_local_dir()?;
     path.push("StoreCache");
     Some(path)
 }
 
-pub fn get_cached_themes_store_path() -> Option<PathBuf>{
+pub fn get_cached_themes_store_path() -> Option<PathBuf> {
     let mut path = get_store_cache_dir()?;
     path.push("themes.json");
     Some(path)
 }
 
-pub fn get_cached_extensions_store_path() -> Option<PathBuf>{
+pub fn get_cached_extensions_store_path() -> Option<PathBuf> {
     let mut path = get_store_cache_dir()?;
     path.push("extensions.json");
     Some(path)
