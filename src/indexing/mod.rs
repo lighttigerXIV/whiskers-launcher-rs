ddb37e1c6a2ee08eee0bfd750647f16858091e50
lighttigerxiv <lighttigerxiv@gmail.com>
2024-04-24 18:10:21 +0100

    add id to indexing app

--- src/indexing/mod.rs
+++ src/indexing/mod.rs
@@ -10,6 +10,7 @@ use crate::{
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AppIndex {
+    pub id: String,
     pub icon_path: Option<String>,
     pub exec_path: String,
     pub name: String,
@@ -17,15 +18,17 @@ pub struct AppIndex {
 
 impl AppIndex {
     pub fn new(
+        id: impl Into<String>,
         icon_path: impl Into<String>,
         exec_path: impl Into<String>,
         name: impl Into<String>,
-    ) -> AppIndex {
-        return AppIndex {
+    ) -> Self {
+        Self {
+            id: id.into(),
             icon_path: Some(icon_path.into()),
             exec_path: exec_path.into(),
             name: name.into(),
-        };
+        }
     }
 }
 
@@ -42,18 +45,18 @@ pub fn get_user_extensions() -> Option<Vec<Manifest>> {
     let mut extensions: Vec<Manifest> = vec![];
 
     if extensions_dir.is_dir() {
-        for entry in WalkDir::new(&extensions_dir){
+        for entry in WalkDir::new(&extensions_dir) {
             let entry = entry.ok()?;
             let entry_name = entry.file_name();
 
             if entry_name == "manifest.json" {
                 let content = fs::read_to_string(&entry.path()).ok()?;
-                if let Ok(manifest) = serde_json::from_str(&content){
+                if let Ok(manifest) = serde_json::from_str(&content) {
                     extensions.push(manifest);
                 }
             }
         }
     }
 
-    return Some(extensions);
+    Some(extensions)
 }
